593c64ecf5275479919441fc9bc12349bb3c0839
Daviniel Nascimento <eng.daviniel@gmail.com>
2023-10-31 00:17:48 -0300

    styled finalizado e corrigido o form

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,36 +1,36 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Progresso do formuário</h1>
+    <div className='App'>
+      <h1>Progresso do Formulário</h1>
+
       <main>
-        <div className="form-group">
-          <label htmlFor="">Nome Completo</label>
+        <div className='form-group'>
+          <label htmlFor=''>Nome Completo</label>
           <input />
         </div>
-        <div className="form-group">
-          <label htmlFor="">E-mail</label>
+        <div className='form-group'>
+          <label htmlFor=''>E-mail</label>
           <input />
         </div>
-        <div className="form-group">
-          <label htmlFor="">Estado Civil</label>
-          <section>
-            <option value="">- selecione...</option>
-            <option value="solteiro">Solteiro</option>
-            <option value="casado">Casa</option>
-            <option value="solteiro">Solteiro</option>
-          </section>
+        <div className='form-group'>
+          <label htmlFor=''>Estado Civil</label>
+          <select>
+            <option value=''>- selecione...</option>
+            <option value='solteiro'>Solteiro</option>
+            <option value='casado'>Casado</option>
+            <option value='divorciado'>Divorciado</option>
+          </select>
         </div>
-        <div className="form-group">
-          <label htmlFor="">Gênero</label>
-          <div className="radios-container">
+        <div className='form-group'>
+          <label htmlFor=''>Gênero</label>
+          <div className='radios-container'>
             <span>
-              <input type="radio" /> Masculino
+              <input type='radio' /> Masculino
             </span>
             <span>
-              <input type="radio" /> Feminino
+              <input type='radio' /> Feminino
             </span>
           </div>
         </div>
