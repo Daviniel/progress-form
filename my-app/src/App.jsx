d93c6be971a99978cca626082139198dfff2c8fc
Daviniel Nascimento <eng.daviniel@gmail.com>
2023-10-30 23:58:31 -0300

    form create

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,7 +3,40 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div className="App">
+      <h1>Progresso do formuário</h1>
+      <main>
+        <div className="form-group">
+          <label htmlFor="">Nome Completo</label>
+          <input />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">E-mail</label>
+          <input />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Estado Civil</label>
+          <section>
+            <option value="">- selecione...</option>
+            <option value="solteiro">Solteiro</option>
+            <option value="casado">Casa</option>
+            <option value="solteiro">Solteiro</option>
+          </section>
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Gênero</label>
+          <div className="radios-container">
+            <span>
+              <input type="radio" /> Masculino
+            </span>
+            <span>
+              <input type="radio" /> Feminino
+            </span>
+          </div>
+        </div>
+        <button>Enviar Formulário</button>
+      </main>
+    </div>
   );
 }
 
